6a7d176a0cc775b8d0bd956e0e649398d6ce185f
Nimish <np4447292@gmail.com>
2026-05-11 11:34:12 +0530

    learning basic of express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello Worlds');
+});
+
+app.get('/about',(req,res)=>{
+    res.send(`Hey there, I am ${req.query.name}`);
+});
+
+app.listen(8000,()=>{
+    console.log('Server is running on port 8000');
+})
