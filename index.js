0effc3e3c1df29eb9cf3be1de77c318bd8a3de18
Nimish <np4447292@gmail.com>
2026-05-13 10:41:55 +0530

    learning rest-api concept

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express=require('express');
+const users=require('./Data');
+
 const app=express();
-
-app.get('/',(req,res)=>{
-    res.send('Hello Worlds');
+const port=8000;
+//server side rendering(SSR)
+app.get('/users',(req,res)=>{
+    const html=`
+    <h1>Users</h1>
+    <ul>
+        ${users.map(user=>`<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `
+    res.send(html);
+});
+//Rest_api
+app.get('/api/users',(req,res)=>{
+    res.json(users);
 });
 
-app.get('/about',(req,res)=>{
-    res.send(`Hey there, I am ${req.query.name}`);
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((u)=>u.id===id);
+    if(!user){
+        return res.status(404).send('User not found');
+    }
+    return res.json(user);
 });
+
 
 app.listen(8000,()=>{
-    console.log('Server is running on port 8000');
+    console.log(`server is running on port ${port}`);
 })
