4fb2350f0c0ed983d8d1298c559c3a5ce20b4d7d
Nimish <np4447292@gmail.com>
2026-05-14 18:47:07 +0530

    learn middle ware and post route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express');
-const users=require('./Data');
+const fs=require('fs');
+const users=require('./RawData.json');
 
 const app=express();
 const port=8000;
+
 //server side rendering(SSR)
 app.get('/users',(req,res)=>{
     const html=`
@@ -25,6 +27,33 @@ app.get('/api/users/:id',(req,res)=>{
         return res.status(404).send('User not found');
     }
     return res.json(user);
+});
+
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+// app.use((req,res,next)=>{
+//     console.log("middleware");
+// })
+//post routes
+app.post('/api/users', (req, res) => {
+    try {
+        const user = req.body;
+
+        const data = fs.readFileSync('./RawData.json', 'utf-8');
+        const users = JSON.parse(data);
+
+        const id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+        user.id = id;
+
+        users.push(user);
+
+        fs.writeFileSync('./RawData.json', JSON.stringify(users, null, 2));
+
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 
